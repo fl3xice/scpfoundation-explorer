eca1208768223bf94daaae79e2dcab62659ddf32
fl3xice <two.fl3xice@yandex.ru>
2023-01-06 15:43:15 +0400

    init

---
+++ src/main.rs
@@ -0,0 +1,413 @@
+pub mod caching;
+pub mod parsing;
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use parsing::{parse_series, ScpObject};
+use std::{
+    env,
+    error::Error,
+    io,
+    thread::{self, Thread},
+    time::{Duration, Instant},
+};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, Gauge, List, ListItem, ListState, Paragraph, Wrap},
+    Frame, Terminal,
+};
+
+#[derive(PartialEq, Clone)]
+enum WindowSelect {
+    Explorer,
+    Objects,
+}
+
+#[derive(PartialEq, Eq, Clone)]
+enum Mode {
+    Default,
+    Search,
+}
+
+#[derive(Clone)]
+struct StatefulList<T> {
+    state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn new() -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items: Vec::new(),
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
+#[derive(Clone)]
+struct AppStates {
+    window: WindowSelect,
+    search: String,
+    mode: Mode,
+    is_load: bool,
+    objects: Option<Vec<ScpObject>>,
+    objects_items: StatefulList<ScpObject>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut app: AppStates = AppStates {
+        window: WindowSelect::Objects,
+        search: String::new(),
+        mode: Mode::Default,
+        is_load: true,
+        objects: None,
+        objects_items: StatefulList::new(),
+    };
+
+    // Collect all arguments
+    let args: Vec<String> = env::args().collect();
+
+    // If arguments have string debug
+    if args.contains(&String::from("debug")) {
+        return Ok(());
+    }
+
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let tick_rate = Duration::from_millis(50);
+    let res = run_app(&mut terminal, &mut app, tick_rate).await;
+
+    // restore terminal
+    disable_raw_mode()?;
+
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+}
+
+fn search(app: &mut AppStates) {
+    if app.is_load {
+        return;
+    }
+
+    let objects: Vec<ScpObject>;
+    if app.search.len() > 0 {
+        objects = app
+            .objects
+            .clone()
+            .unwrap()
+            .iter()
+            .map(|x| x.clone())
+            .filter(|o| {
+                o.get_document_name()
+                    .to_ascii_lowercase()
+                    .contains(&app.search.to_ascii_lowercase())
+                    || o.get_name()
+                        .to_ascii_lowercase()
+                        .contains(&app.search.to_ascii_lowercase())
+            })
+            .collect::<Vec<ScpObject>>();
+    } else {
+        objects = app.objects.clone().unwrap();
+    }
+
+    app.objects_items = StatefulList::with_items(objects);
+}
+
+async fn load(app: &mut AppStates) {
+    if app.objects.is_none() {
+        app.objects = Some(parse_series().await);
+    }
+
+    app.is_load = false;
+    app.objects_items = StatefulList::with_items(app.objects.clone().unwrap());
+}
+
+async fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    app: &mut AppStates,
+    tick_rate: Duration,
+) -> io::Result<()> {
+    let mut last_tick = Instant::now();
+
+    loop {
+        terminal.draw(|f| ui(f, app))?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                match app.mode {
+                    Mode::Default => match key.code {
+                        KeyCode::Esc => {
+                            return Ok(());
+                        }
+
+                        KeyCode::Right | KeyCode::Left => {
+                            if app.mode == Mode::Default {
+                                if WindowSelect::eq(&app.window, &WindowSelect::Explorer) {
+                                    app.window = WindowSelect::Objects;
+                                } else {
+                                    app.window = WindowSelect::Explorer;
+                                }
+                            }
+                        }
+
+                        KeyCode::Up => {
+                            if !app.is_load {
+                                app.objects_items.previous()
+                            }
+                        }
+
+                        KeyCode::Down => {
+                            if !app.is_load {
+                                app.objects_items.next()
+                            }
+                        }
+
+                        KeyCode::Char(c) => {
+                            app.mode = Mode::Search;
+                            app.objects_items.unselect();
+                            app.search.push(c);
+                        }
+
+                        KeyCode::Backspace => {
+                            app.mode = Mode::Search;
+                            app.search.pop();
+                        }
+
+                        _ => {}
+                    },
+
+                    Mode::Search => match key.code {
+                        KeyCode::Esc => {
+                            app.mode = Mode::Default;
+                        }
+
+                        KeyCode::Char(c) => {
+                            app.search.push(c);
+                            search(app)
+                        }
+
+                        KeyCode::Backspace => {
+                            app.search.pop();
+                            search(app);
+                        }
+
+                        KeyCode::Enter => {
+                            app.mode = Mode::Default;
+                            app.window = WindowSelect::Objects;
+                            search(app);
+                            if !app.is_load {
+                                app.objects_items.next();
+                            }
+                        }
+
+                        KeyCode::Right | KeyCode::Left => {
+                            app.mode = Mode::Default;
+                            if WindowSelect::eq(&app.window, &WindowSelect::Explorer) {
+                                app.window = WindowSelect::Objects;
+                            } else {
+                                app.window = WindowSelect::Explorer;
+                            }
+                        }
+
+                        KeyCode::Down => {
+                            app.mode = Mode::Default;
+                            app.window = WindowSelect::Objects;
+                            app.objects_items.next();
+                        }
+
+                        _ => {}
+                    },
+                }
+            }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now();
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut AppStates) {
+    let size = f.size();
+
+    let vertical_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(90), Constraint::Percentage(10)].as_ref())
+        .split(size);
+
+    let mut chunks = Layout::default().direction(Direction::Horizontal);
+
+    if app.window == WindowSelect::Objects {
+        chunks =
+            chunks.constraints([Constraint::Percentage(60), Constraint::Percentage(50)].as_ref());
+    } else {
+        chunks =
+            chunks.constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref());
+    }
+
+    let chunks = chunks.split(vertical_chunks[0]);
+
+    let chunk_left = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(12), Constraint::Percentage(100)])
+        .split(chunks[0]);
+
+    let mut block_with_scp = Block::default().borders(Borders::ALL).title("SCP Объекты");
+    let mut block_explorer = Block::default().borders(Borders::ALL).title("Обзор");
+
+    let block_info = Block::default().borders(Borders::ALL);
+
+    let text = vec![Spans::from(vec![
+        Span::raw("  "),
+        Span::styled("Esc", Style::default().fg(Color::Green)),
+        Span::raw(" "),
+        Span::styled("Выйти", Style::default().add_modifier(Modifier::BOLD)),
+        Span::raw("  "),
+        Span::styled("<- ->", Style::default().fg(Color::Green)),
+        Span::raw(" "),
+        Span::styled(
+            "Выбрать окно",
+            Style::default().add_modifier(Modifier::BOLD),
+        ),
+    ])];
+
+    let info = Paragraph::new(text)
+        .alignment(Alignment::Left)
+        .wrap(Wrap { trim: false })
+        .block(block_info);
+
+    if app.mode == Mode::Default {
+        if WindowSelect::eq(&app.window, &WindowSelect::Explorer) {
+            block_explorer = block_explorer.border_style(Style::default().bg(Color::Blue));
+        } else {
+            block_with_scp = block_with_scp.border_style(Style::default().bg(Color::Blue));
+        }
+    }
+
+    let mut search_block = Block::default()
+        .title("Поиск")
+        .border_type(tui::widgets::BorderType::Rounded)
+        .borders(Borders::ALL);
+
+    if app.mode == Mode::Search {
+        search_block = search_block.border_style(Style::default().bg(Color::Blue));
+    }
+
+    let search_widget = Paragraph::new(Span::styled(
+        &app.search,
+        Style::default().fg(Color::LightGreen),
+    ))
+    .block(search_block);
+
+    let objects: Vec<ListItem> = app
+        .objects_items
+        .items
+        .iter()
+        .map(|o| {
+            ListItem::new(format!(
+                "[{}] {} - {}",
+                o.get_class(),
+                o.get_document_name(),
+                o.get_name()
+            ))
+            .style(Style::default().fg(Color::White))
+        })
+        .collect();
+
+    let scp_list = List::new(objects)
+        .block(block_with_scp)
+        .highlight_style(
+            Style::default()
+                .fg(Color::Blue)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol("☛");
+
+    /* Search Pane */
+    f.render_widget(search_widget, chunk_left[0]);
+    // Render block with the SCP objects
+    if !app.is_load {
+        f.render_stateful_widget(scp_list, chunk_left[1], &mut app.objects_items.state);
+    } else {
+        let mut block = Block::default()
+            .border_style(Style::default())
+            .border_type(tui::widgets::BorderType::Rounded)
+            .borders(Borders::ALL)
+            .title("SCP Объекты (Загружаются)");
+        if app.window == WindowSelect::Objects && app.mode == Mode::Default {
+            block = block.border_style(Style::default().bg(Color::Blue))
+        }
+
+        f.render_widget(block, chunk_left[1]);
+    }
+    // Render block for explore objects
+    f.render_widget(block_explorer, chunks[1]);
+    // Render block for see tips for using app
+    f.render_widget(info, vertical_chunks[1]);
+}
---
+++ src/parsing.rs
@@ -0,0 +1,148 @@
+use core::fmt;
+use std::{
+    fmt::format,
+    fs::{self},
+};
+
+use scraper::{ElementRef, Selector};
+
+const MAX_SERIES: u8 = 8;
+const URL_SERIES: &str = "https://scpfoundation.net/scp-series";
+// const URL_SERIES: &str = "/home/flexice/Downloads/objects.html";
+
+#[derive(Debug, Clone, Copy)]
+pub enum ClassificationScp {
+    None,
+    Safe,
+    Euclid,
+    Keter,
+    Thaumiel,
+    Neutralized,
+    NonStandard,
+}
+
+impl fmt::Display for ClassificationScp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ClassificationScp::Euclid => write!(f, "Евклид"),
+            ClassificationScp::Keter => write!(f, "Кетер"),
+            ClassificationScp::Neutralized => write!(f, "Нейтрализован"),
+            ClassificationScp::None => write!(f, "Отсутствует"),
+            ClassificationScp::Safe => write!(f, "Безопасный"),
+            ClassificationScp::Thaumiel => write!(f, "Таумиэль"),
+            ClassificationScp::NonStandard => write!(f, "Нестандартный класс"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ScpObject {
+    class: ClassificationScp,
+    name: String,
+    id: String,
+}
+
+impl ScpObject {
+    fn new(class: ClassificationScp, name: String, id: String) -> Self {
+        ScpObject { class, id, name }
+    }
+
+    pub fn get_document_name(&self) -> String {
+        String::from(format!("SCP-{}", self.id))
+    }
+
+    pub fn get_name(&self) -> String {
+        let r = format!("{}", &self.name);
+        r.clone()
+    }
+
+    pub fn get_class(&self) -> &ClassificationScp {
+        &self.class
+    }
+
+    pub fn get_id(&self) -> String {
+        let r = format!("{}", &self.id);
+        r.clone()
+    }
+}
+
+pub async fn parse_series() -> Vec<ScpObject> {
+    let mut objects: Vec<ScpObject> = Vec::new();
+
+    let response = reqwest::get(URL_SERIES)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    // let response = fs::read_to_string(URL_SERIES).expect("Should have been able to read the file");
+    let document = scraper::Html::parse_document(&response);
+
+    let paragraph_selector: Selector = scraper::Selector::parse("#page-content>p").unwrap();
+    let paragraphs = document.select(&paragraph_selector).map(|x| x);
+
+    paragraphs.zip(1..101).for_each(|(i, _)| {
+        let childrens: Vec<_> = i
+            .children()
+            .filter(|c| {
+                c.value().is_element()
+                    && c.value().as_element().unwrap().name() == "a"
+                    && c.children().next().unwrap().value().as_text().is_some()
+                    && c.children()
+                        .next()
+                        .unwrap()
+                        .value()
+                        .as_text()
+                        .unwrap()
+                        .starts_with("SCP")
+            })
+            .map(|e| e)
+            .collect();
+
+        childrens.iter().for_each(|c| {
+            let name = c
+                .next_sibling()
+                .unwrap()
+                .value()
+                .as_text()
+                .unwrap()
+                .trim()
+                .strip_prefix("—")
+                .unwrap()
+                .trim();
+
+            let name_d = c.first_child().unwrap().value().as_text().unwrap();
+            let name_d: Vec<_> = name_d.split("-").collect();
+
+            let id = name_d.get(1).unwrap().trim();
+
+            let this = c
+                .prev_sibling()
+                .unwrap()
+                .prev_sibling()
+                .unwrap()
+                .value()
+                .as_element()
+                .unwrap()
+                .attr("alt");
+
+            let class: ClassificationScp = match this {
+                Some(val) => match val {
+                    "na.png" => ClassificationScp::Neutralized,
+                    "safe.png" => ClassificationScp::Safe,
+                    "euclid.png" => ClassificationScp::Euclid,
+                    "keter.png" => ClassificationScp::Keter,
+                    "thaumiel.png" => ClassificationScp::Thaumiel,
+                    "nonstandard.png" => ClassificationScp::NonStandard,
+                    _ => ClassificationScp::None,
+                },
+                None => ClassificationScp::None,
+            };
+
+            objects.push(ScpObject::new(class, name.to_string(), id.to_string()));
+        });
+    });
+
+    objects
+}
